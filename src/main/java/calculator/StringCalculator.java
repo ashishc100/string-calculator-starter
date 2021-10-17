4e0d6753399ed600e9baf0eac20d0616f6f185ee
ashishc100 <92670963+ashishc100@users.noreply.github.com>
2021-10-17 18:25:19 +0530

    Update StringCalculator.java

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,9 +1,39 @@
-package calculator;
+package com.app.core;
 
-class StringCalculator {
+import java.util.StringTokenizer;
 
-    public int add(String input) {
-        return 0;
-    }
+public class StringCalculator {
+
+	public static int Add(String str) {
+
+		int sum = 0;
+		String arr[] = str.split("[ , \n : ; // \\/ | . * %]+");
+
+		for (int i = 0; i < arr.length; i++) {
+			int num = Integer.parseInt(arr[i]);
+			if (num <0) {
+				System.out.println("negative number not allowed");
+				return 0;
+			}
+			else if(num>=1000) {
+				continue;
+			}
+			sum += num;
+		}
+		return sum;
+	}
+		public static int Add(String str, String delimeter) {
+			StringTokenizer st = new StringTokenizer(str, delimeter);
+			int sum = 0;
+			int number =0;
+			while(st.hasMoreTokens()) {
+				number = Integer.parseInt(st.nextToken());
+				sum += number;
+			}
+			
+		
+
+		return sum;
+	}
 
 }
